802ea87a7a34b20d4865d2e0a32bf7560228b444
Genesis McGruder <genesismcgruder@gmail.com>
2024-04-13 19:44:12 -0400

    done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 11
+    while i > 0:
+        i -= 1
+        print(i)
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list = [pow(num,2)for num in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 0
+    while i < 100:
+        i+= 1
+        if (i % 3 == 0 and i % 5 ==0):
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
